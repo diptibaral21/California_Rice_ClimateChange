20a13bdb54185f2c1ea45f20fcf49bc051f9a75a
Dipti Baral <diptibaral21@gmail.com>
2026-02-13 16:38:17 -0800

    Code to download loca future model

---
+++ historical_analysis/loca/C.download_models_2.py
@@ -0,0 +1,19 @@
+#download the LOCA models
+#check the dependencies 
+#this is the code to download the remaining 2 models for tasmin and tasmax 
+
+import xarray as xr
+import intake
+cat = intake.open_esm_datastore(
+    'https://cadcat.s3.amazonaws.com/cae-collection.json'
+)
+#
+var_list=  ['pr', 'rsds', 'tasmax', 'tasmin', 'uas', 'vas', 'wspeed']
+model_list = [ 
+    'IPSL-CM6A-LR', 
+    'MIROC6'
+]
+for model in model_list:
+    for i in range(2,4): # because these two model don't have first 3 variables 
+        ds = xr.open_zarr(cat['LOCA2.UCSD.'+str(model)+'.historical.day.d03'].df.path[i],storage_options={'anon': True})
+        ds.to_netcdf(str(model)+'_historical_r1i1p1f1_'+str(var_list[i])+'.nc')
