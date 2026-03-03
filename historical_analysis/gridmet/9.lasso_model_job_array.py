686f57164a48b4e3a62901ce6c8fda84751fd00b
Dipti Baral <diptibaral21@gmail.com>
2026-03-03 11:30:33 -0800

    Revised historical model code

--- historical_analysis/gridmet/9.lasso_model_job_array.py
+++ historical_analysis/gridmet/9.lasso_model_job_array.py
@@ -17,32 +17,49 @@ from tqdm import tqdm
 
 #file paths
 
-file_path = "/group/moniergrp/dbaral/run_project/input_data/model_input"
+file_path = "/group/moniergrp/dbaral/run_project/input_data/gridmet_hist_model_input"
 save_path = "/group/moniergrp/dbaral/run_project/output_data/historical_model"
 
-# Get trial number from HPC job array
-#trial is the ID of this job in HPC job array
-# for ex. if we submit 100 jobs in SLURM, trial = 1 for the first job and so on
-#each trial represents - one full modeling on the same dataset but with a different random seed for reproducibility 
-#inside each trial, we will still run 100 diff 70/30 splits
+# # Get job_id number from HPC job array
+# #job_id is the ID of this job in HPC job array
+# # for ex. if we submit 1000 jobs in SLURM, job_id = 1 for the first job and so on
+# #each job_id represents - one full modeling on the same dataset but with a different random seed for reproducibility 
+# #inside each job_id, we will still run 1000 diff 70/30 splits
 
-#right now we are using trial = 1 becuase we are just using one dataset
-#In the future , we will run multiple trials for 
-    #13 diff climate models * 2 scenarios = 26 trials
-    #each trials will give a separate pipeline and validation results
+# #right now we are using job_id = 1 becuase we are just using one dataset
+# #In the future , we will have multiple job_id for 
+#     #13 diff climate models * 2 scenarios = 26 trials
+#     #each job_id will give a separate pipeline and validation results
 
-try:
-    trial = int(sys.argv[1])
-except IndexError:
-    raise ValueError("Pass trial as argument: python lasso_model_job_array.py $SLURM_ARRAY_TASK_ID")
+# #define datasets 
+#     #list all gcms and scenarios and create all model-scenario combinations
+
+# gcms = ['ACCESS-CM2', 'CNRM-ESM2-1', 'EC-Earth_3', 'EC-Earth3-Veg', 'GFDL-ESM4', 'INM-CM5-0', 'MPI-ESM1-2-HR', 'MRI-ESM2-0','FGOALS-g3', 'HadGEM3-GC31-LL', 'IPSL-CM6A-LR', 'KACE-1-0-G',  'MIROC6']
+
+# scenarios = ['historical', 'ssp245', 'ssp585']
+
+# datasets = [(gcsm, sc) for gcm in gcms for sc in scenarios]
+
+# #get slurm task id
+# try:
+#     job_id = int(sys.argv[1]) - 1 #zero index
+# except IndexError:
+#     raise ValueError("Pass SLURM_ARRAY_TASK_ID")
+
+# #select model + scenario for this job
+# gcm, scenario = datasets[job_id]
+# model_name =f"{gcm}_{scenario}"
+# print("Running:", gcm, scenario)
 
 #helper functions
 
 def load_and_prepare_data():
     """
-    This function loads the raw dataset and constructs all features.
-    This stpe is done once per trial
-    It does not depend on random splits or lasso.
+    This function loads the raw dataset and constructs all features, including:
+        - squared terms for non-linear effects
+        - county fixed effects 
+        - time trends per county
+    This step is done once per job_id
     """
     
     df = pd.read_csv(file_path+ '/Lasso_Model_Input_Variables_1979_2023_v2.csv')
@@ -66,8 +83,7 @@ def load_and_prepare_data():
     squared_cols = [f"{c}_sq" for c in feature_cols]
     feature_cols += squared_cols
 
-
-    # County fixed effects (0ne-hot encoding)
+    # County fixed effects
     county_dummies = pd.get_dummies(df["county"], prefix="county", drop_first=False).astype(int)
 
     # Time trend within each county
@@ -101,59 +117,71 @@ def remove_outliers_with_cooks_distance(X, Y, df_all):
     mask = cooks_d < threshold
     return X[mask], Y[mask], df_all.loc[mask].copy()
 
-def build_lasso_pipeline(trial):
-    """
-    Constructs a pipeline:
-    a. Standardize features
-    b. Fit lasso with internal cross-validation (LassoCV)
-    We pass the trial as random_state to LassoCV so that different HPC jobs (diff trials) produce reproducible but different model fits.
-    """
-    
-    alphas = [5,4,3,2,1,0.5,0.1,0.05,0.01,0.005,0.001]
-    pipe = Pipeline([
-        ("scaler", StandardScaler()),
-        ("lasso", LassoCV(
-            alphas=alphas, 
-            cv=5, 
-            max_iter=int(1e7), 
-            random_state=trial
-        ))
-    ])
-    return pipe
 
-def run_single_train_test(X, Y, trial):
-    """
-    This fits one lasso model using as single 70/30 split.
-    This step 
-        - gives use one reference model
-        - one set of coefficients
-        - on trian/test performatnce
-        - This is not the final result - its just a baseline
-    """
-    
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.3, random_state=trial
-    )
-    pipe = build_lasso_pipeline(trial)
-    pipe.fit(X_train, y_train)
+# def fit_and_save_full_sample_lasso(X, Y, feature_cols, save_path, model_name, trial=45):
+#     """
+#     Fit full-sample Lasso for a given dataset (trial).
+#     - trial is used as random_state in LassoCV for reproducibility.
 
-    #predictions
+#     Here we fit the full-sample Lasso model and save the following:
+#         - full pipelne(joblib)
+#         - standardized lasso coefficients (CSV)
+#         - unstandardized lasso coefficients (CSV)
+#         - model metadata (alpha, intercept, R2)
+#     """
+#     alphas = [5,4,3,2,1,0.5,0.1,0.05,0.01,0.005,0.001]
+#     pipe = Pipeline([
+#         ("scaler", StandardScaler()),
+#         ("lasso", LassoCV(
+#             alphas=alphas,
+#             cv=5,
+#             max_iter=int(1e6),
+#             random_state=45  
+#         ))
+#     ])
+#     #fit the model
+#     pipe.fit(X, Y)
 
-    y_train_pred = pipe.predict(X_train)
-    y_test_pred = pipe.predict(X_test)
-    
-    #Evaluate
-    r2_train = pipe.score(X_train, y_train)
-    r2_test = pipe.score(X_test, y_test)
+#     #extract components
+#     lasso = pipe.named_steps['lasso']
+#     scaler = pipe.named_steps['scaler']
 
-    rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
-    rmse_test = np.sqrt(mean_squared_error(y_test, y_test_pred))
-    
-    return pipe, r2_train, r2_test, rmse_train, rmse_test, X_train, X_test, y_train, y_test
+#     #coefficients
+#     coef_std = lasso.coef_
+#     coef_unstd = coef_std / scaler.scale_
+#     intercept_unstd = lasso.intercept_ - np.sum((scaler.mean_ / scaler.scale_) * coef_std)
 
-def run_70_30_validation(X, Y, feature_cols, trial, n_iterations=100):
+#     #save full pipeline 
+#     os.makedirs(save_path, exist_ok=True)
+#     joblib.dump(pipe, os.path.join(save_path, f"{model_name}_pipeline.pkl"))
+
+#     #save standardized coefficients
+
+#     coef_std_df = pd.Series(coef_std, index=feature_cols)
+#     coef_std_df.to_csv(os.path.join(save_path, f"{model_name}_standardized_coef.csv"), header = ["coefficient"])
+
+#     #save unstandardized coefficients
+
+#     coef_unstd_df = pd.Series(coef_unstd, index=feature_cols)
+#     coef_unstd_df.to_csv(os.path.join(save_path, f"{model_name}_unstandardized_coef.csv"), header = ["coefficient"])
+
+#     #save metadata
+
+#     metadata = {
+#         "gcm": gcm, 
+#         "scenario": scenario,
+#         "alpha_selected": lasso.alpha_,
+#         "intercept_unstandardized": intercept_unstd,
+#         "r_squared": pipe.score(X,Y)
+#     }
+
+        
+def run_70_30_validation(X, Y, df_filtered, feature_cols, trial, n_iterations=1000):
     """
     This function repeats the 70/30 split 1000 times and fits Lasso each time.
+    70/30 split is done within each country because if we do 70/30 split in full dataset then there is
+    chances of having a whole country missing from the training dataset
+    
     Logic of this Step
         -Instead of just trusting one random split,
          we repate the process 1000 times to assess:
@@ -162,97 +190,121 @@ def run_70_30_validation(X, Y, feature_cols, trial, n_iterations=100):
             - Whether results depend too much on a single split
         Flow inside this function:
             For each iteration i:
-                a. We name a new random split 70/30 split (random_state - i_
+                a. We name a new random split 70/30 split (random_state - i)
                 b. We fit a new LassoCV model
-                c. Then we store:
+                c. Then we save:
+                    - Unstandardized coefficients
+                    - Intercept
+                    - alpha
                     - R2 train/test
                     - RMSE train/test
-                    - Coefficients
-                    - Intercept
-                    - Scaler parameters
+        Saves:
+            1. lasso_1000_model_coefficents.csv
+            2. lasso_1000_validation_metrics.csv
     """
-
-    r2_train_list = []
-    r2_test_list = []
-    rmse_train_list = []
-    rmse_test_list = []
-    coef_list = []
-    intercept_list = []
-    scaler_list = []
-    alphas = [5,4,3,2,1,0.5,0.1,0.05,0.01,0.005,0.001]
+    all_coef_list = []
+    metrics_list = []
     
-    
-    for i in tqdm(range(n_iterations), desc=f"Lasso 70-30 Validation (trail {trial})"):
-    # Perform 70-30 train/test split
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=i) # We dont use trial here, we use i instead, so each split is different
+    for i in tqdm(range(1, n_iterations+1), desc=f"Running Lasso 1000 models"):
+    # Perform county-wise 70-30 train/test split
+        unique_counties = df_filtered['county'].unique()
 
-        pipe = Pipeline([
+        train_indices = []
+        test_indices = []
+
+        for county in unique_counties:
+            county_mask = df_filtered['county'] == county
+            county_indices = df_filtered[county_mask].index
+
+            train_index_c, test_index_c = train_test_split(county_indices, test_size=0.3, random_state = i )
+            train_indices.extend(train_index_c)
+            test_indices.extend(test_index_c)
+        X_train = X.loc[train_indices]
+        X_test = X.loc[test_indices]
+        y_train = Y.loc[train_indices]
+        y_test = Y.loc[test_indices]
+
+        #fit LassoCV
+        alphas = [5,4,3,2,1,0.5,0.1,0.05,0.01,0.005,0.001]
+        pipe_lasso_70_30_split = Pipeline([
             ("scaler", StandardScaler()), 
             ("lasso", LassoCV(
                 alphas=alphas, 
                 cv=5, 
-                max_iter=int(1e9),
+                max_iter=int(1e6),
                 random_state=45  #fixed seed so only the split changes
             ))
         ])
+        #fit model
 
-        pipe.fit(X_train, y_train)
+        pipe_lasso_70_30_split.fit(X_train, y_train)
 
-        #predictions
+        lasso = pipe_lasso_70_30_split.named_steps['lasso']
+        scaler = pipe_lasso_70_30_split.named_steps['scaler']
 
-        y_train_pred = pipe.predict(X_train)
-        y_test_pred = pipe.predict(X_test)
+        #convert to unstandardized coefficients
+        coef_std = lasso.coef_
+        coef_unstd = coef_std/scaler.scale_
+
+        intercept_unstd = (lasso.intercept_ - np.sum((scaler.mean_/scaler.scale_) * coef_std))
+
+        #save coefficients (long format)
+        coef_df_iteration = pd.DataFrame({
+            'feature': feature_cols, 
+            'coefficient': coef_unstd, 
+            'iteration': i
+        })
+
+        intercept_row = pd.DataFrame({
+            'feature': ['intercept'],
+            'coefficient': [intercept_unstd],
+            'iteration': i
+        })
+
+        coef_df_iteration = pd.concat(
+            [coef_df_iteration, intercept_row], 
+            ignore_index=True
+        )
+
+        all_coef_list.append(coef_df_iteration)
+        
+
+        #predictions and validation metrics
+
+        y_train_pred = pipe_lasso_70_30_split.predict(X_train)
+        y_test_pred = pipe_lasso_70_30_split.predict(X_test)
 
         #rmse
-        rmse_train_val = np.sqrt(mean_squared_error(y_train, y_train_pred))
-        rmse_test_val = np.sqrt(mean_squared_error(y_test, y_test_pred))
-
-        rmse_train_list.append(rmse_train_val)
-        rmse_test_list.append(rmse_test_val)
+        rmse_train = np.sqrt(mean_squared_error(y_train, y_train_pred))
+        rmse_test = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
         #R2
-        r2_train_val = pipe.score(X_train, y_train)
-        r2_test_val = pipe.score(X_test, y_test)
+        r2_train = pipe_lasso_70_30_split.score(X_train, y_train)
+        r2_test = pipe_lasso_70_30_split.score(X_test, y_test)
 
-        r2_train_list.append(r2_train_val)
-        r2_test_list.append(r2_test_val)
-        
-        #coefficients 
-        lasso = pipe.named_steps['lasso']
-        scaler = pipe.named_steps['scaler']
-        
-        coef_list.append(lasso.coef_)
-        intercept_list.append(lasso.intercept_)
-        scaler_list.append(scaler)
 
-    #Aggregate coefficients within this trial
-    coef_array = np.vstack(coef_list)
-    mean_coef_std = np.mean(coef_array, axis=0)
-    mean_intercept_std = np.mean(intercept_list)
+        #save csv with R2 and RMSE for each iteration
 
-    mean_scale = np.mean([s.scale_ for s in scaler_list], axis=0)
-    mean_mean = np.mean([s.mean_ for s in scaler_list], axis=0)
+        metrics_list.append({
+            'iteration': i, 
+            'alpha_selected': lasso.alpha_,
+            'R2_train': r2_train,
+            'R2_test': r2_test,
+            'RMSE_train': rmse_train,
+            'RMSE_test': rmse_test
+        })
+    #After the loop save everything
 
-    mean_coef_unstd = mean_coef_std / mean_scale
-    mean_intercept_unstd = mean_intercept_std - np.sum((mean_mean / mean_scale) * mean_coef_std)
+    final_coef_df = pd.concat(all_coef_list, ignore_index = True)
+    metrics_df = pd.DataFrame(metrics_list)
 
-    validation_results = {
-        "trial": trial,
-        "r2_train": np.array(r2_train_list),
-        "r2_test": np.array(r2_test_list),
-        "rmse_train": np.array(rmse_train_list), 
-        "rmse_test": np.array(rmse_test_list),
-        "mean_r2_train": float(np.mean(r2_train_list)),
-        "mean_r2_test": float(np.mean(r2_test_list)),
-        "mean_rmse_train": float(np.mean(rmse_train_list)),
-        "mean_rmse_test": float(np.mean(rmse_test_list)),
-        "mean_coef_standardized": mean_coef_std,
-        "mean_coef_unstandardized": mean_coef_unstd,
-        "mean_intercept_unstandardized": mean_intercept_unstd,
-        "feature_cols": feature_cols
-    }
-    return validation_results
+    final_coef_df.to_csv(os.path.join(save_path, 'lasso_1000_models_coefficients.csv'), index=False)
 
+    metrics_df.to_csv(
+        os.path.join(save_path, "lasso_1000_model_validation_metrics.csv"), index=False)
+    return final_coef_df, metrics_df 
+    
+            
 #main workflow
 
 if __name__ == "__main__":
@@ -260,37 +312,20 @@ if __name__ == "__main__":
     # Load data (once per trial)
     df_all, feature_cols = load_and_prepare_data()
 
-    X = df_all[feature_cols].values
-    Y = df_all["yield_kg_ha"].values
+    X = df_all[feature_cols]
+    Y = df_all["yield_kg_ha"]
 
     #Remove outliers (once per trial)
     X, Y, df_filtered = remove_outliers_with_cooks_distance(X, Y, df_all)
 
-    #Fit one baseline model
-    pipe, r2_train, r2_test, *_ = run_single_train_test(X, Y, trial)
+    # #Fit one full-sample lasso model
+    # trial = 45
+    # fit_and_save_full_sample_lasso(X, Y, feature_cols, save_path, model_name, trial=45)
 
-    #save this single model
-    artifacts = {
-        "pipeline": pipe, 
-        "feature_cols_final": feature_cols, 
-        "counties_train": sorted(df_filtered["county"].unique()),
-        "year_min": df_all['year'].min()
-    }
+    #Run repeated validation (1000 splits)
 
-    joblib.dump(
-        artifacts, 
-        os.path.join(save_path, f"Lasso_pipeline_train_{trial}.joblib")
-    )
+    validation_results = run_70_30_validation(X, Y, df_filtered, feature_cols, trial, n_iterations=1000)
 
-    #Run repeated validation (100 splits)
-
-    validation_results = run_70_30_validation(X, Y, feature_cols, trial, n_iterations=100)
-
-    joblib.dump(
-        validation_results, 
-        os.path.join(save_path, f"lasso_70_30_trial_{trial}.joblib")
-    )
-
-    print(f"Finished trial {trial}")
+    print("Finished running the model")
     
     
